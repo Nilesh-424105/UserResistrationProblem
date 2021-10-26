e0e2a4a31b7dc53261fb44a1e3749ac44c2b5dec
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-26 17:16:49 +0530

    ADDED: Welcome Message

---
+++ com/userregistration/UserRegistrationTest.java
@@ -0,0 +1,10 @@
+package com.userregistration;
+
+public class UserRegistrationTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To User Regisration Program");
+	}
+
+}
